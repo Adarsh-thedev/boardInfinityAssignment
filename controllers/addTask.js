b166be2d49dd9e573f3e57b59c71d3045b26f416
Adarsh-thedev <adarshpittu@gmail.com>
2021-03-24 10:02:36 +0530

    more customized errors

--- controllers/addTask.js
+++ controllers/addTask.js
@@ -4,6 +4,16 @@ exports.addTask = (req, res) => {
   const task = new Task(req.body);
   task.save((err, task) => {
     if (!task || err) {
+      if (err.code === 11000) {
+        return res
+          .status(400)
+          .json("Please make sure the task is not dupicate");
+      } else if (err.message) {
+        return res.status(400).json({
+          error: err.message,
+        });
+      }
+
       return res.status(400).json({
         message:
           "Unable to add task, please make sure you have added the required paramenters",
