48c20da56ca7067df7910e3c373412ec7a28cbdd
Adarsh-thedev <adarshpittu@gmail.com>
2021-03-24 08:44:57 +0530

    task schema

---
+++ models/task.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const taskSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    maxlength: 100,
+  },
+  description: {
+    type: String,
+    required: true,
+    maxlength: 2000,
+  },
+  creator: {
+    type: String,
+    required: true,
+    maxlength: 32,
+  },
+  duration: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("Task", taskSchema);
