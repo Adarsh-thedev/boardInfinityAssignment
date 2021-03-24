cda5c8308bc6aff3e433ee267f87343828b97e1d
Adarsh-thedev <adarshpittu@gmail.com>
2021-03-24 13:46:01 +0530

    deploying

--- controllers/getTasks.js
+++ controllers/getTasks.js
@@ -2,7 +2,7 @@ const Task = require("../models/task");
 
 exports.getTasks = (req, res) => {
   Task.find().exec((err, tasks) => {
-    if (!tasks.length || err) {
+    if (!tasks || !tasks.length || err) {
       return res.status(404).json({ error: "No tasks found" });
     }
     res.json(tasks);
