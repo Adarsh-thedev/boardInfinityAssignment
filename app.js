99ebc76d8f252b8cf0c2cfce6832ea1a887aeefa
Adarsh-thedev <adarshpittu@gmail.com>
2021-03-24 09:45:21 +0530

    getting everything combined

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
+require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
-
+const cors = require("cors");
 const app = express();
+
+const taskRoutes = require("./routes/task");
+
+//middlewares
+app.use(express.json());
+app.use(cors());
 
 mongoose.connect(
   "mongodb://localhost:27017/bi-test",
@@ -13,6 +21,9 @@ mongoose.connect(
     console.log("DB Connected");
   }
 );
+
+//my routes
+app.use("/api", taskRoutes);
 
 app.listen(8000, () => {
   console.log("app is running");
