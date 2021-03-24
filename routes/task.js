8e2de8b041a58e7b0d7d763bb114d106621b84a6
Adarsh-thedev <adarshpittu@gmail.com>
2021-03-24 08:55:02 +0530

    task routes

--- routes/task.js
+++ routes/task.js
@@ -4,4 +4,7 @@ const router = express.Router();
 const { addTask } = require("../controllers/addTask");
 const { getTasks } = require("../controllers/agetTasks");
 
+router.post("/api/add", addTask);
+router.get("/api/list", getTasks);
+
 module.exports = router;
