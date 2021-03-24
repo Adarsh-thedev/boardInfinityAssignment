3e0f6c2551c23f3feb72a351303cb1162772e52f
Adarsh-thedev <adarshpittu@gmail.com>
2021-03-24 16:08:09 +0530

    fixing deployment issues

--- models/task.js
+++ models/task.js
@@ -1,36 +1,35 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const taskSchema = new Schema(
-  {
-    name: {
-      type: String,
-      unique: true,
-      required: true,
-      maxlength: 100,
-    },
-    description: {
-      type: String,
-      required: true,
-      maxlength: 2000,
-    },
-    creator: {
-      type: String,
-      required: true,
-      maxlength: 32,
-    },
-    duration: {
-      type: Number,
-      required: true,
-      trim: true,
-    },
+const taskSchema = new Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+    maxlength: 100,
   },
-  { timestamp: true }
-);
+  description: {
+    type: String,
+    required: true,
+    maxlength: 2000,
+  },
+  creator: {
+    type: String,
+    required: true,
+    maxlength: 32,
+  },
+  duration: {
+    type: Number,
+    required: true,
+    trim: true,
+  },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
 
-taskSchema.index(
-  { createdAt: 1 },
-  { expireAfterSeconds: taskSchema.duration * 60 }
-);
+taskSchema.index({ createdAt: 1 }, { expireAfterSeconds: duration * 60 });
 
 module.exports = mongoose.model("Task", taskSchema);
